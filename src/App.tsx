fd687e591f9f05491940c751102a5ccc545db077
Renataversos <renataverso13@gmail.com>
2026-03-30 22:34:44 -0400

    Fix: Remove subfolder routing for root domain deploy

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ const AnimatedRoutes = () => {
 export default function App() {
   return (
     <DataProvider>
-      <Router basename="/renataverso">
+      <Router>
         <ScrollToTop />
         <div className="flex-1 flex flex-col min-h-screen font-sans">
           <Header />
