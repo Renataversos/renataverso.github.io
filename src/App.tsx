b1da58499a1060e0d2cd59d06a5cd7030b37d0de
renataverso13 <renataverso13@github.com>
2026-04-01 00:51:09 -0400

    Fix: Corrigindo base path do Vite e basename do Router para GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ const AnimatedRoutes = () => {
 export default function App() {
   return (
     <DataProvider>
-      <Router>
+      <Router basename="/renataverso.github.io">
         <ScrollToTop />
         <div className="flex-1 flex flex-col min-h-screen font-sans">
           <Header />
